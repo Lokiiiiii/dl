2c95e1307807a14505db41a38646adc150662a53
Lokiiiiii <loki94@cmu.edu>
2018-09-08 21:36:23 -0400

    Saturday Night

--- 1/hw1/hw1.py
+++ 1/hw1/hw1.py
@@ -22,11 +22,10 @@ function whose implementation has been provided.
 import numpy as np
 import os
 
+"""https://hips.seas.harvard.edu/blog/2013/01/09/computing-log-sum-exp/
+"""
 
-def LogSumExp(vector):
-"""
-https://hips.seas.harvard.edu/blog/2013/01/09/computing-log-sum-exp/
-"""
+def LogSumExp(vector):	
 	mx = np.amax(vector)
 	shifted = vector - mx
 	exp = np.exp(shifted)
@@ -35,218 +34,247 @@ https://hips.seas.harvard.edu/blog/2013/01/09/computing-log-sum-exp/
 	return exp, sumexp, result
 
 def load_mnist_data_file(path):
-    raise NotImplementedError
+	train_data = np.load(path+"train_data.npy")
+	val_data = np.load(path+"val_data.npy")
+	test_data = np.load(path+"test_data.npy")
+	train_labels = np.load(path+"train_labels.npy")
+	val_labels = np.load(path+"val_labels.npy")
+	test_labels = np.load(path+"test_labels.npy")
+	return train_data, train_labels, val_data, val_labels, test_data, test_labels
+
 
 
 class Activation(object):
-    """ Interface for activation functions (non-linearities).
+	""" Interface for activation functions (non-linearities).
 
-        In all implementations, the state attribute must contain the result, i.e. the output of forward (it will be tested).
-    """
+		In all implementations, the state attribute must contain the result, i.e. the output of forward (it will be tested).
+	"""
 
-    def __init__(self):
-        self.state = None
+	def __init__(self):
+		self.state = None
 
-    def __call__(self, x):
-        return self.forward(x)
+	def __call__(self, x):
+		return self.forward(x), self.derivative()
 
-    def forward(self, x):
-        raise NotImplemented
+	def forward(self, x):
+		raise NotImplemented
 
-    def derivative(self):
-        raise NotImplemented
+	def derivative(self):
+		raise NotImplemented
 
 
 class Identity(Activation):
-    """ Identity function (already implemented).
-     """
+	""" Identity function (already implemented).
+	 """
 
-    def __init__(self):
-        super(Identity, self).__init__()
+	def __init__(self):
+		super(Identity, self).__init__()
 
-    def forward(self, x):
-        self.state = x
-        return x
+	def forward(self, x):
+		self.state = np.ones((x.shape))
+		return x
 
-    def derivative(self):
-        return 1.0
+	def derivative(self):
+		return self.state
+#		return 1.0
 
 
 class Sigmoid(Activation):
-    """ Implement the sigmoid non-linearity """
+	""" Implement the sigmoid non-linearity """
 
-    def __init__(self):
-        super(Sigmoid, self).__init__()
+	def __init__(self):
+		super(Sigmoid, self).__init__()
 
-    def forward(self, x):
-        raise NotImplemented
+	def forward(self, x):
+		self.state = 1/(1+np.exp(-x))
+		return self.state
 
-    def derivative(self):
-        raise NotImplemented
-
+	def derivative(self):
+		return self.state*(1-self.state) 
 
 class Tanh(Activation):
-    """ Implement the tanh non-linearity """
+	""" Implement the tanh non-linearity """
 
-    def __init__(self):
-        super(Tanh, self).__init__()
+	def __init__(self):
+		super(Tanh, self).__init__()
 
-    def forward(self, x):
-        raise NotImplemented
+	def forward(self, x):
+		self.state = np.tanh(x)
+		return self.state
 
-    def derivative(self):
-        raise NotImplemented
+	def derivative(self):
+		return 1-self.state**2
 
 
 class ReLU(Activation):
-    """ Implement the ReLU non-linearity """
+	""" Implement the ReLU non-linearity """
 
-    def __init__(self):
-        super(ReLU, self).__init__()
+	def __init__(self):
+		super(ReLU, self).__init__()
 
-    def forward(self, x):
-        raise NotImplemented
+	def forward(self, x):
+		self.state = ( x*(x>0), x)
+		return self.state[0]
 
-    def derivative(self):
-        raise NotImplemented
+	def derivative(self):
+		x = self.state[1]
+		return 1*(x>0)
 
 
 # CRITERION
 
 
 class Criterion(object):
-    """ Interface for loss functions.
-    """
+	""" Interface for loss functions.
+	"""
 
-    def __init__(self):
-        self.logits = None
-        self.labels = None
-        self.loss = None
+	def __init__(self):
+		self.logits = None
+		self.labels = None
+		self.loss = None
 
-    def __call__(self, x, y):
-        return self.forward(x, y)
+	def __call__(self, x, y):
+		return self.forward(x, y)
 
-    def forward(self, x, y):
-        raise NotImplemented
+	def forward(self, x, y):
+		raise NotImplemented
 
-    def derivative(self):
-        raise NotImplemented
+	def derivative(self):
+		raise NotImplemented
 
 
 class SoftmaxCrossEntropy(Criterion):
-    def __init__(self):
-        super(SoftmaxCrossEntropy, self).__init__()
-        self.sm = None
+	def __init__(self):
+		super(SoftmaxCrossEntropy, self).__init__()
+		self.sm = None
 
-    def forward(self, x, y):
-    	batch_result = []
-    	self.memory = []
-    	for b in range(x.shape[0]):
-    		exp, sumexp, logsumexp = LogSumExp(x[b])
-    		negsumproduct = -np.sum(x[b]*y[b])
-    		t2 = np.sum(y[b]*logsumexp)
-    		result_per_example = t2 + negsumproduct
-    		batch_result.append(result_per_example)
-    		self.memory.append(-y[b] + (exp/sumexp))
-    	return(np.array(batch_result))
+	def forward(self, x, y):
+		batch_result = []
+		self.state = []
+		for b in range(x.shape[0]):
+			exp, sumexp, logsumexp = LogSumExp(x[b])
+			negsumproduct = -np.sum(x[b]*y[b])
+			t2 = np.sum(y[b]*logsumexp)
+			result_per_example = t2 + negsumproduct
+			batch_result.append(result_per_example)
+			self.state.append(-y[b] + (exp/sumexp))
+		return(np.array(batch_result))
 
-    def derivative(self):
-    	return(np.array(self.memory))
+	def derivative(self):
+		return(np.array(self.state))
 
 
 class BatchNorm(object):
-    def __init__(self, fan_in, alpha=0.9):
-        self.alpha = alpha
-        self.eps = 1e-8
-        self.x = None
-        self.norm = None
-        self.out = None
+	def __init__(self, fan_in, alpha=0.9):
+		self.alpha = alpha
+		self.eps = 1e-8
+		self.x = None
+		self.norm = None
+		self.out = None
 
-        # The following attributes will be tested
-        self.var = np.ones((1, fan_in))
-        self.mean = np.zeros((1, fan_in))
+		# The following attributes will be tested
+		self.var = np.ones((1, fan_in))
+		self.mean = np.zeros((1, fan_in))
 
-        self.gamma = np.ones((1, fan_in))
-        self.dgamma = np.zeros((1, fan_in))
+		self.gamma = np.ones((1, fan_in))
+		self.dgamma = np.zeros((1, fan_in))
 
-        self.beta = np.zeros((1, fan_in))
-        self.dbeta = np.zeros((1, fan_in))
+		self.beta = np.zeros((1, fan_in))
+		self.dbeta = np.zeros((1, fan_in))
 
-        # inference parameters
-        self.running_mean = np.zeros((1, fan_in))
-        self.running_var = np.ones((1, fan_in))
+		# inference parameters
+		self.running_mean = np.zeros((1, fan_in))
+		self.running_var = np.ones((1, fan_in))
 
-    def __call__(self, x, eval=False):
-        return self.forward(x, eval)
+	def __call__(self, x, eval=False):
+		return self.forward(x, eval)
 
-    def forward(self, x, eval=False):
-        raise NotImplemented
+	def forward(self, x, eval=False):
+		raise NotImplemented
 
-    def backward(self, delta):
-        raise NotImplemented
+	def backward(self, delta):
+		raise NotImplemented
 
 
 def random_normal_weight_init(d0, d1):
-	return np.random.randn(do,d1)
+	return np.random.randn(d0,d1)
 
 def zeros_bias_init(d):
 	return np.zeros((1,d))
 
 class MLP(object):
-    """ A simple multilayer perceptron
-    """
+	""" A simple multilayer perceptron
+	"""
 
-    def __init__(self, input_size, output_size, hiddens,
-                 activations, weight_init_fn, bias_init_fn,
-                 criterion, lr, momentum=0.0, num_bn_layers=0):
-        # Don't change this -->
-        self.train_mode = True
-        self.num_bn_layers = num_bn_layers
-        self.bn = num_bn_layers > 0
-        self.nlayers = len(hiddens) + 1
-        self.input_size = input_size
-        self.output_size = output_size
-        self.activations = activations
-        self.criterion = criterion
-        self.lr = lr
-        self.momentum = momentum
-        # <---------------------
+	def __init__(self, input_size, output_size, hiddens,
+				 activations, weight_init_fn, bias_init_fn,
+				 criterion, lr, momentum=0.0, num_bn_layers=0):
+		# Don't change this -->
+		self.train_mode = True
+		self.num_bn_layers = num_bn_layers
+		self.bn = num_bn_layers > 0
+		self.nlayers = len(hiddens) + 1
+		self.input_size = input_size
+		self.output_size = output_size
+		self.activations = activations
+		self.criterion = criterion
+		self.lr = lr
+		self.momentum = momentum
+		# <---------------------
 
-        # Don't change the name of the following class attributes,
-        # the autograder will check against these attributes
-        self.W = None
-        self.dW = None
-        self.b = None
-        self.db = None
-        # if batch norm, add batch norm parameters
-        if self.bn:
-            self.bn_layers = None
+		# Don't change the name of the following class attributes,
+		# the autograder will check against these attributes
+		self.lsizes = [input_size] + hiddens + [output_size]
+		self.W = [weight_init_fn(m,n) for m,n in zip(self.lsizes[:-1],self.lsizes[1:])]
+		self.b = [bias_init_fn(m) for m in self.lsizes[1:]]
+#		self.dW = [np.zeros_like(matrix) for matrix in self.W]
+#		self.db = [np.zeros_like(matrix) for matrix in self.b]
+		# if batch norm, add batch norm parameters
+		if self.bn:
+			self.bn_layers = None
+		# Feel free to add any other attributes useful to your implementation (input, output, ...)
+		self.loss = SoftmaxCrossEntropy()
 
-        # Feel free to add any other attributes useful to your implementation (input, output, ...)
+	def forward(self, x):
+		self.state = [(x,)] + [[]]*len(self.W)
+		for i in range(len(self.W)):
+			self.state[i+1] = ( self.activations[i]( self.state[i][0].dot(self.W[i]) + self.b[i]) )
+#			self.state[i+1] = ( self.activations[i]( np.hstack((self.state[i][0],np.ones((self.state[i][0].shape[0],1)))).dot( np.vstack((self.W[i],self.b[i])) ) ) )
+		return self.state[-1][0]
 
-    def forward(self, x):
-        raise NotImplemented
+	def zero_grads(self):
+		self.gradients = [[] for l in self.lsizes]
+#		self.gradients = [np.zeros((l,1)) for l in self.lsizes]
 
-    def zero_grads(self):
-        raise NotImplemented
+	def step(self)  :
+		self.W = [self.W[i] - self.lr*self.dW[i] for i in range(len(self.W))]
+		self.b = [self.b[i] - self.lr*self.db[i] for i in range(len(self.b))]
 
-    def step(self):
-        raise NotImplemented
+	def backward(self, labels):
+		loss_value = self.loss.forward(self.state[-1][0], labels)
+		dloss = self.loss.derivative()
+		self.zero_grads()
+		self.gradients[-1] = dloss.T
+		g=len(self.gradients)-2
+		while g>=0:
+			self.gradients[g] = self.W[g].dot(np.multiply(self.state[g+1][-1].T, self.gradients[g+1]))		
+			g-=1
+		self.db = [self.gradients[i+1].T*self.state[i+1][1] for i in range(len(self.b))]
+		self.dW = [self.state[i][0].T.dot(self.db[i]) for i in range(len(self.b))]
 
-    def backward(self, labels):
-        raise NotImplemented
 
-    def __call__(self, x):
-        return self.forward(x)
+	def __call__(self, x):
+		return self.forward(x)
 
-    def train(self):
-        self.train_mode = True
+	def train(self):	
+		self.train_mode = True
 
-    def eval(self):
-        self.train_mode = False
+
+	def eval(self):
+		self.train_mode = False
 
 
 def get_training_stats(mlp, dset, nepochs, batch_size):
-    raise NotImplemented
-
+	for e in range(nepochs):
+		pass
 
---
+++ 1/hw1/try.py
@@ -0,0 +1,40 @@
+import hw1 as hw
+import numpy as np
+
+td,tl,vd,vl,tsd,tsl = hw.load_mnist_data_file("./../data/")
+init=0
+no=1
+weight_init=hw.random_normal_weight_init
+bias_init=hw.zeros_bias_init
+
+mlp = hw.MLP(784, 10, [], [hw.Identity()], weight_init, bias_init,
+                      hw.SoftmaxCrossEntropy(), 0.008, momentum=0.0,
+                      num_bn_layers=0)
+z = mlp.forward(td[init:init+no])
+
+labels = np.array([np.zeros(10) for l in tl])
+for i in range(tl.shape[0]):
+	labels[i][tl[i]]=1
+
+#print("Actual:{} Prediction:{} Loss:{}".format(tl[init:init+no],z.argmax(1),loss))
+#print("Predict_Accuracy:{} Loss_zero:{}".format(tl[init:init+no]==z.argmax(1),loss==0))
+#print("Loss_accuracy:{}".format((tl[init:init+no]==z.argmax(1))==(loss==0)))
+
+
+
+mlp1 = hw.MLP(784, 10, [32], [hw.Sigmoid(), hw.Identity()], weight_init, bias_init, hw.SoftmaxCrossEntropy(), 0.008, momentum=0.0, num_bn_layers=0)
+mlp1.forward(td[init:init+no])
+mlp1.backward(labels[init:init+no])
+
+
+ref=hw
+hw1=hw
+mlp2 = hw.MLP(784, 10, [64, 32], [ref.Sigmoid(), ref.Sigmoid(), hw1.Identity()],
+                      weight_init, bias_init, ref.SoftmaxCrossEntropy(), 0.008,
+                      momentum=0.0, num_bn_layers=0)
+
+mlp3 = hw1.MLP(784, 10, [32, 32, 32, 32, 32],
+                      [ref.Sigmoid(), ref.Sigmoid(), ref.Sigmoid(), ref.Sigmoid(),
+                       ref.Sigmoid(), hw1.Identity()],
+                      weight_init, bias_init, ref.SoftmaxCrossEntropy(), 0.008,
+                      momentum=0.0, num_bn_layers=0)
